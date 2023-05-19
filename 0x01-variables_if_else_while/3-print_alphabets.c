e0f4af3b9528242b7ffce79215b9adfb0c69786f
foash-111 <mr2221@fayoum.edu.eg>
2023-05-19 14:43:44 +0000

    keep going please

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with alphapets in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 97 ; n <= 122 ;)
+	{
+	putchar(n++);
+	}
+	for (n = 65 ; n <= 90 ;)
+	{
+	putchar(n++);
+	}
+	putchar('\n');
+	return (0);
+}
