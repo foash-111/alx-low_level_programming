eeeb1ae5d2320295b5db3ff89c7a5560de2ad80d
foash-111 <mr2221@fayoum.edu.eg>
2023-05-19 13:29:52 +0000

    keep going please

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,17 +21,17 @@ int main(void)
 	if (mod > 5)
 	{
 	printf("Last digit of %d", n);
-	printf("is %d and is greater than 5", mod);
+	printf(" is %d and is greater than 5", mod);
 	}
 	if (mod == 0)
 	{
 	printf("Last digit of %d", n);
-	printf("is %d and is 0", mod);
+	printf(" is %d and is 0", mod);
 	}
 	if (mod < 6 && mod != 0)
 	{
 	printf("Last digit of %d", n);
-	printf("is %d and is less than 6 and not 0", mod);
+	printf(" is %d and is less than 6 and not 0", mod);
 	}
 	return (0);
 }
