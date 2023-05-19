57bad0bc05768a34e250f2d79fd694798e964c9c
foash-111 <mr2221@fayoum.edu.eg>
2023-05-19 15:23:28 +0000

    keep going please

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with alphapets in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 122 ; n > 97 ; n--)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
