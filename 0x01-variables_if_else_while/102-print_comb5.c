8f40f6d1e07961431a793b3651d7d039660b9599
foash-111 <mr2221@fayoum.edu.eg>
2023-05-19 14:59:10 -0700

    i'm still going ahead :)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 	for (j = 48 ; j <= 57 ; j++)
 	{
-	for (k = i ; k <= 57 ; k++)
+	for (k = 48 ; k <= 57 ; k++)
 	{
-	for (l = j + 1 ; l <= 57 ; l++)
+	for (l = 48 ; l <= 57 ; l++)
 	{
 	if (i * 10 + j < k * 10 + l)
 	{
