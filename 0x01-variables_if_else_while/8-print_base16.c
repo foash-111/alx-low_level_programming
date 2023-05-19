a514e7c62a55543cdb466e164ecd843a21cd67e2
foash-111 <mr2221@fayoum.edu.eg>
2023-05-19 15:34:26 +0000

    keep going please

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with alphapets in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+	putchar(n);
+	}
+	for (n = 97 ; n <= 102 ; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
