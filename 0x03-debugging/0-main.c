6f3e33c5a787c0a2d8347b1671da5917a1578283
foash-111 <mr2221@fayoum.edu.eg>
2023-05-25 23:52:33 -0700

    i'm here ya clab

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: A C program that prints with srand function
- *
- * Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
-void positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else
-		(n == 0) printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
-}
 int main(void)
 {
 	int n;
