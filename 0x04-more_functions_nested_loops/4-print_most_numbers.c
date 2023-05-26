ba4c8ee9d8e42989b28ba6710cbc81077e50adc5
foash-111 <mr2221@fayoum.edu.eg>
2023-05-26 13:11:25 -0700

    hhh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,10 +3,12 @@
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
- * continue when == 2 || when == 4
+ * Do not print 2 and 4
  *
- * This function prints the numbers from 0 to 9, followed by a new line.
- */
+ * only use _putchar twice in your code
+ *
+ * always return 0 when success
+*/
 
 void print_numbers(void)
 {
