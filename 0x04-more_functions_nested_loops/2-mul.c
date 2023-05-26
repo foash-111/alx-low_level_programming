8b77b673ae80dfd4e0de4f84025306e6cdcb4623
foash-111 <mr2221@fayoum.edu.eg>
2023-05-26 10:23:51 -0700

    ya mosahel elhal ya rb

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @c: the character to check
+ *
+ * Return: the multiplication of them
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
