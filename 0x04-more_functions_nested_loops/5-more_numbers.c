8c7a3e293acd672e3f631a764e79fde8618010a2
foash-111 <mr2221@fayoum.edu.eg>
2023-05-27 08:13:39 +0000

    zay elreeh

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers from 0 to 14
+ *
+ * only use _putchar three times in your code
+ *
+ * always return 0 when success
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 48 ; i <= 49 ; i++)
+	{
+	for (j = 48 ; j <= 57 ; j++)
+	{
+	if (i == 48)
+	_putchar(j);
+	else
+	{
+	_putchar(i);
+	_putchar(j);
+	if (j == 52)
+	break;
+	}
+	}
+	}
+	_putchar('\n');
+}
