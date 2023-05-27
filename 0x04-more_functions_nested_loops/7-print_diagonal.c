3632bcc79d5d68b4197a7fb6dc6917353fdab673
foash-111 <mr2221@fayoum.edu.eg>
2023-05-27 10:48:09 +0000

    alx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,20 @@ void print_diagonal(int n)
 {
 
 int i, j;
-
+if (n > 0)
+{
 for (j = 0 ; j < n ; j++)
 {
 for (i = 0 ; i < j ; i++)
 {
-putchar(32);
+_putchar(32);
 }
-putchar (92);
-putchar('\n');
+_putchar (92);
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
 }
 }
