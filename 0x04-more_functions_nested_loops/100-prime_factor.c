61487174d287b97f793aa3474c2d06c5465853af
foash-111 <mr2221@fayoum.edu.eg>
2023-05-28 12:07:40 +0000

    i'm here ya wlad els5a

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * larg_prime - return the large prime factor
+ *
+ * @number: the numer we want to return the large prime factor to it
+ *
+ * Return: the large prime factor
+ *
+*/
+
+int larg_prime(long long number)
+{
+int larger = 0;
+
+while (number % 2 == 0)
+{
+number = number / 2;
+larger = 2;
+}
+while (number % 3 == 0)
+{
+number = number / 3;
+larger = 3;
+}
+for (int i = 5 ; i <= 5000 ; i += 2)
+{
+while (number % i == 0)
+{
+number /= i;
+larger = i;
+if (number == 1)
+break;
+}
+if (number == 1)
+break;
+}
+if (larger < 2)
+{
+larger = number;
+}
+return (larger);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the large prime factor
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("%d\n", larg_prime(612852475143));
+return (0);
+}
