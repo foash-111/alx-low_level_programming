d48419d59acb5a56c15757ba31b70b10921843f0
foash-111 <mr2221@fayoum.edu.eg>
2023-06-09 13:47:41 -0700

    never give up

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that printsthe program name
+ *
+ * @argc: first argument to show the number of commands
+ * argc = arg command
+ *
+ * @argv: second argument to show argument array
+ * argv = arg vector or `array`
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char const *argv[])
+{
+	
+printf("%d\n", argc);
+
+return (0);
+}
