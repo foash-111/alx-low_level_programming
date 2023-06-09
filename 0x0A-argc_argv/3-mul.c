87a11ecce3eeec65218a729c8e6c9cd7dc3d3d9f
foash-111 <mr2221@fayoum.edu.eg>
2023-06-09 16:23:52 -0700

    ya ana ya 5alty

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that printsthe program name
+ *
+ * @argc: first argument to show the number of commands
+ * argc = arg command
+ *
+ * @argv:second parameter
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+int num1, num2;
+int mul = 1;
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+if (argc == 3)
+{
+
+mul = num1 * num2;
+
+printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+
+return (0);
+}
