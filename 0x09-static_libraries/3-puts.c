238658af61bc9e2de279a0fb0fdd835a1c705bc5
foash-111 <mr2221@fayoum.edu.eg>
2023-06-09 08:32:40 -0700

    ya mosaheel

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: the first parameter
+ *
+ * Return: none
+*/
+
+void _puts(char *str)
+{
+int count;
+int i;
+
+i = 0;
+count = 0;
+
+while (*(str + count) != '\0')
+{
+count++;
+}
+
+while (i < count)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
+}
