9e5abdb8e5a3c21b002b419dcb15cef12c2dc6bf
foash-111 <mr2221@fayoum.edu.eg>
2023-06-09 15:10:15 -0700

    keep going

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that printsthe program name
+ *
+ * @argc: first argument to show the number of commands
+ * argc = arg command
+ *
+ * @argv:second parameter
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+int count;
+
+for (count = 1; count < argc; count++)
+
+{
+printf("%s\n", argv[count]);
+}
+
+return (0);
+}
