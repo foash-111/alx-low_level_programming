2489ead25db0a90d1aebf7e27f034a23c65392ed
foash-111 <mr2221@fayoum.edu.eg>
2023-06-09 15:19:40 -0700

    keep going

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -16,11 +16,17 @@ int main(int argc, char *argv[])
 {
 int count;
 
+if (argc > 1)
+{
 for (count = 1; count < argc; count++)
 
 {
 printf("%s\n", argv[count]);
 }
-
+}
+else
+{
+printf("\n");
+}
 return (0);
 }
