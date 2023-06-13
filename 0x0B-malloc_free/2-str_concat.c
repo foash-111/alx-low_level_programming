9ba1d978fd8327ce74138d18a87c1ed7ed522072
foash-111 <mr2221@fayoum.edu.eg>
2023-06-13 13:23:03 -0700

    rooh

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - function init==array
+ *
+ * @s1:the address of the array1
+ *
+ * @s2:the address of the array2
+ *
+ * Return:char
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+int i;
+int size1, size2;
+char *ptr;
+
+size1 = 0;
+size2 = 0;
+i = 0;
+while (1)
+{
+size1++;
+if (*(s1 + i) == '\0')
+{break; }
+i++;
+}
+i = 0;
+while (1)
+{
+if (*(s2 + i) == '\0')
+{break; }
+size2++;
+i++; }
+ptr = malloc(sizeof(char) * (size1 + size2));
+if (ptr == 0 || (size1 + size2) == 0)
+{return (0); }
+i = 0;
+while (i < (size1 + size2))
+{
+if (i < size1)
+ptr[i] = s1[i];
+else
+ptr[i] = s1[i - size2];
+i++;
+}
+return (ptr);
+}
