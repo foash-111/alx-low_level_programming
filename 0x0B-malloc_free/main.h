02c9fd7c040e4b486435bd0228e8e2537bc6382f
foash-111 <mr2221@fayoum.edu.eg>
2023-06-13 05:28:05 -0700

    ya gabl ma yehzak reeh

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -15,5 +15,12 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
