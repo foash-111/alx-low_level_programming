3079b6cf166ce3436c7bbe64b13876b6fd6a49c2
foash-111 <mr2221@fayoum.edu.eg>
2023-06-13 11:40:49 -0700

    rooh

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include"main.h"
+
+/**
+ * create_array - function init==array
+ *
+ * @size:size of the array
+ *
+ * @c:the init value
+ *
+ *
+ *
+ * Return:char
+*/
+
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *ptr;
+
+i = 0;
+
+
+ptr = malloc(sizeof(char) * size);
+
+if (ptr == 0 || size == 0)
+{
+return (0);
+}
+
+while (i < size)
+{
+ptr[i] = c;
+i++;
+}
+
+return (ptr);
+
+}
