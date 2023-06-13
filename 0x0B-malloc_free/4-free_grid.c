2720337e4b404811eee3667947414344316fa0b7
foash-111 <mr2221@fayoum.edu.eg>
2023-06-13 15:18:22 -0700

    rooh

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function init==array
+ *
+ * @grid:the address of the array1
+ *
+ * @height:the address of the array2
+ *
+ * Return:nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+int i;
+
+i = 0;
+
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
+}
