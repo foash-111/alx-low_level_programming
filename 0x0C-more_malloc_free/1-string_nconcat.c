52dc994a8bc14d24d7358e807b1e42c60238844b
foash-111 <mr2221@fayoum.edu.eg>
2023-06-15 16:34:26 -0700

    ya rabb qawina

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 
 /**
+ * strlen - calc length
+ *
+ *@str:address of the array
+ *
+ * Return: int -length-.
+*/
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i, size;
+
+	size = 0;
+	i = 0;
+
+while (1)
+{
+if (*(str + i) == '\0')
+{
+	break;
+}
+
+size++;
+i++;
+}
+
+return (size);
+
+}
+
+/**
  * *string_nconcat - function init==array
  *
  * @s1:the address of the array1
@@ -15,39 +45,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i,  size1, size2;
+unsigned int i, j, size1, size2;
 char *ptr;
 
-size1 = 0, size2 = 0, i = 0;
+size1 = 0, size2 = 0, i = 0, j = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (1)
+
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+
+if (n < size2)
 {
-	size1++;
-if (*(s1 + i) == '\0')
-{break; }
-i++;
+	j = (size1 + n);
+	ptr = malloc(sizeof(char) * (size1 + n + 1));
 }
-i = 0;
-while (1)
-{
-if (*(s2 + i) == '\0')
-{break; }
-size2++;
-i++;
-}
-if (n <= size2)
-ptr = malloc(sizeof(char) * (size1 + n));
 	else
-	ptr = malloc(sizeof(char) * (size1 + size2));
-if (ptr == 0 || (size1 + n) == 0)
+{
+	j = (size1 + size2);
+	ptr = malloc(sizeof(char) * (size1 + size2 + 1));
+}
+	if (ptr == 0)
 {return (0); }
 i = 0;
-while (i < (size1 + n))
+
+while (i < j)
 {
-if (i < size1 - 1)
+if (i < size1)
 ptr[i] = s1[i];
 else
 ptr[i] = s2[i - size1];
