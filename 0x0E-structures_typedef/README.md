e048538406f30b886203a1f811aa8763126cc55d
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 08:35:44 -0700

    keep going please ... never give up ,champ

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+READ me care fully please
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - struct describe a dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
