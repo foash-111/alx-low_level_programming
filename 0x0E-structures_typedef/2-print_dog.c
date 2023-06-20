09b855a9265321a4e12c6864398b3fbf7098fff1
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 11:47:21 -0700

    never give up .. keep going

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 
 {
 if (d->name == NULL)
-printf("(nil)");
+printf("Name: (nil)");
 else
 {
 printf("Name: %s\n", d->name);
@@ -28,7 +28,7 @@ printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 }
 if (d->owner == NULL)
-printf("(nil)");
+printf("Owner: (nil)");
 else
 {
 printf("Owner: %s\n", d->owner);
