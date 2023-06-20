5df90b34db3d72e2cc82adf81ddbff794fd644fc
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 00:56:46 -0700

    still alive still keep going never give up

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifndef SIZE
+#define SIZE 1024
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+
+#endif
+
