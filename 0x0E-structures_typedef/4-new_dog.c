2c76d6af89e2c2fafb8d2b155f1a86238944f966
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 15:54:37 -0700

    i'm sorry

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,8 +14,8 @@ char *_strcpy(char *dest, char *src);
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *max_dog;
-	int name_l = 0, own_l = 0;
+dog_t *max_dog;
+int name_l = 0, own_l = 0;
 
 if (name != NULL && owner != NULL)
 {
