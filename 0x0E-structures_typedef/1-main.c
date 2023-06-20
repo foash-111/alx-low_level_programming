24ec477fdb15b8825a0237ae069f754d36aaf6a0
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 09:11:02 -0700

    keep going please ... never give up ,champ

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -13,10 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+struct dog *ptr;
 
-d->name = name;
-d->age = age;
-d->owner = owner;
+ptr = d;
+
+ptr->name = name;
+ptr->age = age;
+ptr->owner = owner;
 }
 
 /**
