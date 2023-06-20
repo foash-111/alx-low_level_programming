8ff09a1a5b21aae5515ebdf5a629525adaddb03a
foash-111 <mr2221@fayoum.edu.eg>
2023-06-20 13:19:38 -0700

    never give up .. keep going

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - delete struct from the memory
+ *
+ * @d: my struct
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->age);
+free(d->owner);
+}
+
+}
