fe20ad13ecac605afaf30423a75d4368d2dd532b
foash-111 <mr2221@fayoum.edu.eg>
2023-06-23 16:26:31 -0700

    i'm here again

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ va_start(ptr, n);
 
 for (i = 0; i < n; i++)
 {
-if (i == n-1 || separator == NULL)
+if (i == n - 1 || separator == NULL)
 printf("%d", va_arg(ptr, int));
 else
 printf("%d%s", va_arg(ptr, int), separator);
