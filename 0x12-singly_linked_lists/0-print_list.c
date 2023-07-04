d7d6de9e68c0f2b20729b7d857a5b1758fe7d387
foash-111 <mr2221@fayoum.edu.eg>
2023-07-04 06:10:27 -0700

    let's try again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,21 +15,18 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
-
-while (h != NULL)
+while (h)
 {
 if (h->str == NULL)
-printf("[0] (nil)\n");
+	printf("[0] (nil)");
 else
-printf("[%d] %s\n", h->len, h->str);
+	printf("[%d] %s", h->len, h->str);
 
-h = h->next;
 count++;
+h = h->next;
 
 }
 
 return (count);
-
-
 
 }
