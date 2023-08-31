51af03862b36d406563369f0e0ac49476e3bc21d
foash-111 <mr2221@fayoum.edu.eg>
2023-08-31 08:47:09 -0700

    hello foash it's a long to see you again

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
- */
+ * _puts_recursion -  function that prints a string
+ *
+ *@s : pointer to the start of the string
+ *
+ * Return : nothing >> always >> 0 (succeed)
+*/
+
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+if (*s == '\0')
+	return;
+else
+{
+	_putchar(*s);
+_puts_recursion(s + 1);
 
-	else
-		_putchar('\n');
 }
-
+}
