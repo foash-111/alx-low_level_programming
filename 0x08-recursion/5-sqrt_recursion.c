3136a3222e36dba8508bb4d24ef0508884f38cb2
foash-111 <mr2221@fayoum.edu.eg>
2023-09-02 08:33:55 -0700

    after 2 days trying

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,4 @@
 #include "main.h"
-/**
- *_sqrt_recursion - find square root
- *
- *@n:the number we need to find the square root to it
- *
- *Return: the result
-*/
-
-
-int _sqrt_recursion(int n)
-{
-if (n == 0)
-	return (0);
-else if (n == 1)
-	return (1);
-else if (n > 1)
-{
-
-
-}
-
-}
 
 /**
  *_sqrt_recursion_helper - help
@@ -38,6 +16,7 @@ int _sqrt_recursion_helper(int n, int start, int end)
 {
 int mid = start + (end - start) / 2;
 int square = mid * mid;
+
 if (start > end)
 	return (-1);
 if (square == n)
@@ -46,4 +25,26 @@ if (square > n)/*169 > 25*/
 	return (_sqrt_recursion_helper(n, 1, mid - 1));
 if (square < n)/*16 < 25*/
 	return (_sqrt_recursion_helper(n, mid + 1, end));
+return (-1);
+}
+
+/**
+ *_sqrt_recursion - find square root
+ *
+ *@n:the number we need to find the square root to it
+ *
+ *Return: the result
+*/
+
+
+int _sqrt_recursion(int n)
+{
+if (n == 0 || n == 1)
+	return (n);
+else if (n < 1)
+	return (-1);
+else
+{
+return (_sqrt_recursion_helper(n, 1, n));
+}
 }
