e6dfe42997e71caed8ea6e4f7344956fd7070936
foash-111 <mr2221@fayoum.edu.eg>
2023-09-03 02:32:07 -0700

    let's try again .. foash came back

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,57 @@
-#include "main.h"
-
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+#include <stdio.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
+ * palindrome_helper - help the functhin plndrm
  *
- * Return: 1 if it is, 0 it's not
- */
-int is_palindrome(char *s)
+ * @s:string we check
+ * @start:the start point from 0
+ * @end:the end point
+ *
+ * Return: 1 on success
+*/
+
+
+int palindrome_helper(char *s, int start, int end)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+if (start > end)
+return (1);
+
+if (s[start] == s[end])
+return (palindrome_helper(s, start + 1, end - 1));
+else
+return (0);
 }
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
+ * _strlen_recursion - function that returns the length of a string
  *
- * Return: length of the string
- */
+ *@s: the start point of the string
+ *
+ *Return: the lenght of the string
+*/
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
+int count = 0;
 
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
+if (*s)
 {
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
-		return (1);
-	return (check_pal(s, i + 1, len - 1));
+return ((count + 1) + _strlen_recursion(s + 1));
+}
+return (count);
+
+}
+/**
+ * is_palindrome - check if the string is palindrome or not
+ * @s: the string we check
+ *Return: 1 on success
+*/
+int is_palindrome(char *s)
+{
+int end = _strlen_recursion(s);
+
+if (end == 0)
+return (1);
+
+return (palindrome_helper(s, 0, end - 1));
 }
