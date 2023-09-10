fe8aab8094a07874fa7a754b54aa1d95497c49e2
foash-111 <mr2221@fayoum.edu.eg>
2023-09-10 14:46:30 +0300

    let's try again .. foash came back

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,23 +1,17 @@
-#include<stdio.h>
+#include"stdio.h"
 
 /**
- * main - Entry point
- *
- * Description: A C program that printsthe program name
- *
- * @argc: first argument to show the number of commands
- * argc = arg command
- *
- * @argv: second argument to show argument array
- * argv = arg vector or `array`
- *
- * Return: Always 0 (Success)
+ * entry point - main function
+ * @argc - num of argument
+ * @argv - array of pointers to arguments
+ * Return: (0) on success
 */
 
-int main(int argc, char const *argv[])
+(void) int main (int argc, char *argv)
 {
-	(void)argc;
-printf("%s\n", argv[0]);
+printf("%s\n", argv[0])
+
 
 return (0);
+
 }
