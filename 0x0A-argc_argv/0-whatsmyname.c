07147d8d9c6651159baaa871392591984e9f471b
foash-111 <mr2221@fayoum.edu.eg>
2023-09-10 05:32:51 -0700

    let's try again .. foash came back

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,30 +1,16 @@
 #include"stdio.h"
 
 /**
-<<<<<<< HEAD
  * main - main function
  * @argc: num of argument
  * @argv: array of pointers to arguments
  * Return: (0) on success
 */
 
-int main(int argc,  char **argv)
+int main(int argc,  char *argv[])
 {
 	(void) argc;
 printf("%s\n", argv[0]);
-=======
- * entry point - main function
- * @argc - num of argument
- * @argv - array of pointers to arguments
- * Return: (0) on success
-*/
-
-(void) int main (int argc, char *argv)
-{
-printf("%s\n", argv[0])
-
->>>>>>> fe8aab8094a07874fa7a754b54aa1d95497c49e2
-
 
 return (0);
 
