82fe26cfbc28eb254e650d9a5cc04bb630fd2374
foash-111 <mr2221@fayoum.edu.eg>
2023-09-11 07:56:35 -0700

    let's try again .. foash came back

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,10 @@
 int main(int argc, char *argv[])
 {
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-int result = num1 * num2;
 
 if (argc == 3)
 {
-	printf("%d\n", result);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
 else
 {
