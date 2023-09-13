59005e9dbfda556184ed3b26b3a3577fc915b3be
foash-111 <mr2221@fayoum.edu.eg>
2023-09-13 00:35:47 -0700

    let's try again .. foash came back

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 					_putchar('0' + result % 10);
 				}
 			else
-				if (result < 10)
+				if ((i) * (j + 1) < 10)
 				{
 					_putchar('0' + result);
 					_putchar(',');
