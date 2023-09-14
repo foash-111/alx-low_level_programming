e81b6f8bef1a4994db7271cdb33fcd92093a47d0
foash-111 <mr2221@fayoum.edu.eg>
2023-09-14 00:47:17 -0700

    let's try again .. foash came back

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,34 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
- * print_number - a function that prints an integer
- *
- * @number: the numer we want to print
- *
- * Return: none
- *
-*/
+ * print_number - print number.
+ *@n: the number we want to print
+ * Return: void.
+ */
 
 void print_number(int n)
+{
+	int temp = 0, count = 0;
+
+	while (n)
+	{
+		/*987*/
+		/*9//90 + 8 == 98//980 + 7 == 987*/
+		temp = (n % 10) + (temp * 10);
+		/*temp *= 10;//90//980//9870 --error--*/
+		n = n / 10;
+		count++;
+	}
+
+	while (--count)/*987*/
+	{
+		/*/7//8//9*/
+		putchar('0' + temp % 10);
+		/*98////9//0*/
+		temp /= 10;
+
+	}
+
+	printf("%d\n", temp);
+
+}
