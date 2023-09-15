64e3ec039785ae38653d31c26e7a3a18f80ffadb
foash-111 <mr2221@fayoum.edu.eg>
2023-09-15 05:47:53 -0700

    let's try again .. foash came back

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	 int n1 = 1, n2 = 2;
+	int sum = 0;
+	int result = 0;
+
+	while (sum < 4000000)
+	{
+		/*sum += i + (i - 1);*/
+		/*/ 3   1    2*/
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+
+		if (sum % 2 == 0)
+			result += sum;
+
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
