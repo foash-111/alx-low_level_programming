41797a01f587247e5e705a404a039284f6875cc9
foash-111 <mr2221@fayoum.edu.eg>
2023-09-15 02:59:51 -0700

    let's try again .. foash came back

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,16 +10,16 @@ int main(void)
 	long long int i, n1 = 1, n2 = 2;
 	long long int sum = 0;
 
+	printf("1, 2, ");
 	for (i = 1; i <= 50; i++)
 	{
 
 		sum = n1 + n2;
 		n1 = n2;
 		n2 = sum;
-		printf("%lld\n", sum);
-
+		printf("%lld, ", sum);
 
 	}
-
+	putchar('\n');
 	return (0);
 }
