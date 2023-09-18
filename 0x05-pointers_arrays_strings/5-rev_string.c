6c22ea937abf3659c75f3ee336d86b75a8a391e2
foash-111 <mr2221@fayoum.edu.eg>
2023-09-18 08:02:05 -0700

    let's try again .. foash came back

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of the string
+ *
+ * @s: the first parameter
+ *
+ * Return: int
+*/
+
+int _strlen(char *s)
+{
+int count;
+
+count = 0;
+
+while (s[count])
+{
+count++;
+}
+if (s == NULL)
+return (0);
+else
+return (count);
+}
+
+
+
+/**
+ * rev_string - prints a string followed by a new line
+ *
+ * @s: the first parameter
+ *
+ * Return: none
+*/
+
+void rev_string(char *s)
+{
+int i;
+
+i = _strlen(s);
+
+while (i > 0)
+{
+_putchar(s[i - 1]);
+i--;
+}
+
+_putchar('\n');
+}
