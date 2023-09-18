822cb460330137561143741c741356ea777e7f26
foash-111 <mr2221@fayoum.edu.eg>
2023-09-18 07:33:29 -0700

    let's try again .. foash came back

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * _strlen - return the length of the string
+ *
+ * @s: the first parameter
+ *
+ * Return: int
+*/
+
+int _strlen(char *s)
+{
+int count;
+
+count = 0;
+
+while (s[count])
+{
+count++;
+}
+if (s == NULL)
+        return (0);
+        else
+return (count);
+}
+
+
+
+/**
  * print_rev - prints a string followed by a new line
  *
  * @s: the first parameter
@@ -25,4 +51,3 @@ i--;
 s[len - 1] = '\0';
 _putchar('\n');
 }
-
