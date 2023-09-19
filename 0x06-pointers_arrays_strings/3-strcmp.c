3d33f29e15bb805fb67677929704b68fe9dc189b
foash-111 <mr2221@fayoum.edu.eg>
2023-09-19 08:23:40 -0700

    strcmp function done alhamdullah

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+* _strcmp - compare two strings with each other
+*@s1: pointer to the base string
+* @s2: pointer to the string i want to concat it
+* Return: s1 > s2 ? 1 , s1 < s2 ? -1 : 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, s1_len = 0, s2_len = 0;
+
+	while (s1[s1_len])
+	{
+		s1_len++;
+	}
+	while (s2[s2_len])
+	{
+		s2_len++;
+	}
+
+	if (s1_len > s2_len)
+		return (1);
+	else if (s1_len < s2_len)
+		return (-1);
+	else
+		return (0);
+}
