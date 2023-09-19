a261f537e0a4e4f9116fae70498a04aac9209061
foash-111 <mr2221@fayoum.edu.eg>
2023-09-19 08:45:59 -0700

    strcmp function done alhamdullah

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int sum1 = 0, sum2 = 0, s1_len = 0, s2_len = 0;
+	int long sum1 = 0, sum2 = 0, s1_len = 0, s2_len = 0;
 
 	while (s1[s1_len])
 	{
-		sum1 += (int)s1[s1_len];
+		sum1 += (int)s1[s1_len] - 48;
 		s1_len++;
 	}
 	while (s2[s2_len])
 	{
-		sum2 += (int)s2[s2_len];
+		sum2 += (int)s2[s2_len] - 48;
 		s2_len++;
 	}
 
