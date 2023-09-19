a7f03d88da84eb0f009e0db31e8830ff231636b8
foash-111 <mr2221@fayoum.edu.eg>
2023-09-19 00:55:40 -0700

    atoi function done alhamdullah

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdbool.h>
+
+/**
+* _atoi - convert string to integer
+*
+* @s: pointer to the string that i want
+* to convert
+*
+* Return: the integer value of the number
+* that was in the string
+*
+*
+*/
+
+
+int _atoi(char *s)
+{
+	int i = 0;
+	bool flag = 0;
+	int x = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+			flag = !flag;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				x = ((int)s[i] - 48) + (x * 10);
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+
+	if (flag)
+		x = -x;
+
+	return (x);
+}
