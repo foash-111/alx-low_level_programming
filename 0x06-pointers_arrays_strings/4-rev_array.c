553c459f3beec4562c80e843276cc31fe469d3a7
foash-111 <mr2221@fayoum.edu.eg>
2023-09-19 09:56:04 -0700

    rev_array function done alhamdullah

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+* reverse_array - reverse array
+* @a: pointer to the start of the array
+* @n: number of elements
+* return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int temp = 0, i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[n - 1];
+		a[n - 1] = a[i];
+		a[i] = temp;
+		n--;
+	}
+}
