4ac8823fa6799e39631f1c5c80af889714dde05b
foash-111 <mr2221@fayoum.edu.eg>
2023-10-02 14:01:09 -0700

    argv print file name function done alhamdullah

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *@argc: arguments counter
+ *@argv: arguments vector
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%S", argv[argc - 1]);
+}
