4b80ed74166eadf8b3758cdf462381aba9a984ca
foash-111 <mr2221@fayoum.edu.eg>
2023-10-02 14:07:01 -0700

    argv print file name function done alhamdullah

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  * Return: Always 0 (Success)
 */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-printf("%S", argv[argc - 1]);
+printf("%s\n", argv[0]);
+return (0);
 }
