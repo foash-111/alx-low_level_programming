899800ee113e2c7b2ac71f7665230c13d11df7e4
foash-111 <mr2221@fayoum.edu.eg>
2023-10-04 08:11:49 -0700

    str_concatenate with malloc function done alhamdullah

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,14 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int len1 = 0, len2 = 0, i = 0;
 
+	if (s1 == NULL)
+		len1 = 0;
+	else
 	while (s1[len1])
 		len1++;
+	if (s2 == NULL)
+		len2 = 0;
+	else
 	while (s2[len2])
 		len2++;
 
