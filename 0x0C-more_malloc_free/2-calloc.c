46d43385a64439ec6d945e5c0c6eb343426f45fd
foash-111 <mr2221@fayoum.edu.eg>
2023-10-08 14:32:44 -0700

    _calloc function done alhamdullah

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,17 @@
  * @size: size of data type
  * Return: (void *)
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 	unsigned int i = 0;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	ptr = malloc(size * nmemb);
 
-	if (ptr == NULL || nmemb == 0 || size == 0)
+	if (ptr == NULL)
 	return (NULL);
 
 	while (i < nmemb)
