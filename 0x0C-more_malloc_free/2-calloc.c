8d70e1e4331ee3c11c773897f912edcdb730c2cd
foash-111 <mr2221@fayoum.edu.eg>
2023-10-09 12:24:11 -0700

    array of integers USING MALLOC function done alhamdullah

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,12 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (0);
 
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-	return (NULL);
+	return (0);
 
 	while (i < (nmemb * size))
 	{
