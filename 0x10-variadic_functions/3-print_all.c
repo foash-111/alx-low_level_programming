1e030bf4d65106e1b313bc3f7e1762f2b237b4aa
foash-111 <mr2221@fayoum.edu.eg>
2023-10-13 07:38:12 -0700

    a function that prints anything.using variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include<string.h>
+#include"variadic_functions.h"
+
+/**
+ * print_char - ...
+ * @pa: pointer to argument
+ * @last: check flag to print ", " or not
+ * Return: (void)
+ */
+
+void print_char(va_list pa, int last)
+{
+	printf("%c", va_arg(pa, int));
+	if (!last)
+		printf(", ");
+}
+
+/**
+ * print_string - ...
+ * @pa: pointer to argument
+ * @last: check flag to print ", " or not
+ * Return: (void)
+ */
+void print_string(va_list pa, int last)
+{
+	char *ptr;
+
+	ptr = va_arg(pa, char *);
+	if (ptr == NULL)
+	printf("(nil)");
+	else
+	printf("%s", ptr);
+
+	if (!last)
+		printf(", ");
+}
+
+/**
+ * print_int - ...
+ * @pa: pointer to argument
+ * @last: check flag to print ", " or not
+ * Return: (void)
+ */
+void print_int(va_list pa, int last)
+{
+	printf("%d", va_arg(pa, int));
+	if (!last)
+		printf(", ");
+}
+/**
+ * print_float - ...
+ * @pa: pointer to argument
+ * @last: check flag to print ", " or not
+ * Return: (void)
+ */
+
+void print_float(va_list pa, int last)
+{
+	printf("%f", va_arg(pa, double));
+	if (!last)
+		printf(", ");
+}
+
+/**
+ * print_all - variadic function to print anything
+ * @format: pointer to argument
+ * @...: ...
+ * Return: (void)
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list pa;
+	int len = 0, i = 0, j = 0;
+
+	op_t identifier[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'i', print_int},
+		{'f', print_float}
+	};
+
+	len = strlen(format);
+	va_start(pa, format);
+
+	while (i < len)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (identifier[j].ch == format[i])
+				identifier[j].f(pa, i == len - 1);
+			j++;
+		}
+		i++;
+	}
+
+	va_end(pa);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * operator - struct
+ * @ch: char
+ * @f: pointer to function
+ */
+typedef struct operator
+{
+	char ch;
+	void (*f)(va_list, int);
+}op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
