91ba7c03ba1b7f9246243eab4c145fc0db59495b
foash-111 <mr2221@fayoum.edu.eg>
2023-10-14 14:11:07 +0300

    a function that prints anything.using variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,64 @@
 #include<stdarg.h>
 #include"variadic_functions.h"
 
+
+/**
+ * print_char - ...
+ * @pa: pointer to argument
+ * @str: check flag to print ", " or not
+ * Return: (void)
+ */
+
+void print_char(char *str, va_list pa)
+{
+	printf("%s%c", str, va_arg(pa, int));
+
+}
+
+/**
+ * print_string - ...
+ * @pa: pointer to argument
+ * @str: check flag to print ", " or not
+ * Return: (void)
+ */
+void print_string(char *str, va_list pa)
+{
+	char *ptr;
+
+	ptr = va_arg(pa, char *);
+	if (ptr == NULL)
+	{
+	printf("%s%s", str, "(nil)");
+	return;
+	}
+	printf("%s%s", str, ptr);
+}
+
+
+/**
+ * print_int - ...
+ * @pa: pointer to argument
+ * @str: check flag to print ", " or not
+ * Return: (void)
+ */
+void print_int(char *str, va_list pa)
+{
+	printf("%s%i", str, va_arg(pa, int));
+
+}
+/**
+ * print_float - ...
+ * @pa: pointer to argument
+ * @str: check flag to print ", " or not
+ * Return: (void)
+ */
+
+void print_float(char *str, va_list pa)
+{
+	printf("%s%f", str, va_arg(pa, double));
+
+}
+
 /**
  * print_all - variadic function to print anything
  * @format: pointer to argument
@@ -12,40 +70,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list pa;
-	int i = 0;
+	int i = 0, j = 0;
 	char *str = "";
-	char *ptr;
 
-if (format != NULL)
-{
+	op_t identifier[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"f", print_float}
+	};
+
 	va_start(pa, format);
 
-	while (format[i])
+	while (format[i] && format != NULL)
 	{
-		switch (format[i])
-		{
-		case 'c':
-			printf("%s%c", str, va_arg(pa, int));
-			break;
-		case 's':
-		ptr = va_arg(pa, char *);
-		printf("%s%s", str, ptr ? ptr : "(nil)");
-			break;
-		case 'f':
-		printf("%s%f", str, va_arg(pa, double));
-			break;
-		case 'i':
-		printf("%s%d", str, va_arg(pa, int));
-			break;
-		default:
-		i++;
-		continue;
-		}
-	str = ", ";
+		j = 0;
 
+
+		while (j < 4)
+		{
+			if (identifier[j].ch[0] == format[i])
+			{
+				identifier[j].f(str, pa);
+				str = ", ";
+			}
+			
+
+			j++;
+		}
 		i++;
 	}
-}
+
 	va_end(pa);
-	printf("\n");
+	putchar('\n');
 }
