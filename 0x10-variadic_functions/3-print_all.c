6d235f44cff07cfede46fd0514821e042a185152
foash-111 <mr2221@fayoum.edu.eg>
2023-10-14 12:50:01 +0300

    a function that prints anything.using variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,64 +2,6 @@
 #include<stdarg.h>
 #include"variadic_functions.h"
 
-
-/**
- * print_char - ...
- * @pa: pointer to argument
- * @str: check flag to print ", " or not
- * Return: (void)
- */
-
-void print_char(char *str, va_list pa)
-{
-	printf("%s%c", str, va_arg(pa, int));
-
-}
-
-/**
- * print_string - ...
- * @pa: pointer to argument
- * @str: check flag to print ", " or not
- * Return: (void)
- */
-void print_string(char *str, va_list pa)
-{
-	char *ptr;
-
-	ptr = va_arg(pa, char *);
-	if (ptr == NULL)
-	{
-	printf("%s%s", str, "(nil)");
-	return;
-	}
-	printf("%s%s", str, ptr);
-}
-
-
-/**
- * print_int - ...
- * @pa: pointer to argument
- * @str: check flag to print ", " or not
- * Return: (void)
- */
-void print_int(char *str, va_list pa)
-{
-	printf("%s%i", str, va_arg(pa, int));
-
-}
-/**
- * print_float - ...
- * @pa: pointer to argument
- * @str: check flag to print ", " or not
- * Return: (void)
- */
-
-void print_float(char *str, va_list pa)
-{
-	printf("%s%f", str, va_arg(pa, double));
-
-}
-
 /**
  * print_all - variadic function to print anything
  * @format: pointer to argument
@@ -70,36 +12,37 @@ void print_float(char *str, va_list pa)
 void print_all(const char * const format, ...)
 {
 	va_list pa;
-	int i = 0, j = 0;
+	int i = 0;
 	char *str = "";
-
-	op_t identifier[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"f", print_float}
-	};
+	char *ptr;
 
 	va_start(pa, format);
 
-	while (format[i] && format != NULL)
+	while (format[i] && format)
 	{
-		j = 0;
-
-
-		while (j < 4)
+		switch (format[i])
 		{
-			if (identifier[j].ch[0] == format[i])
-			{
-				identifier[j].f(str, pa);
-			}
-			str = ", ";
-
-			j++;
+		case 'c':
+			printf("%s%c", str, va_arg(pa, int));
+			break;
+		case 's':
+		ptr = va_arg(pa, char *);
+		printf("%s%s", str, ptr ? ptr : NULL);
+			break;
+		case 'f':
+		printf("%s%f", str, va_arg(pa, double));
+			break;
+		case 'i':
+		printf("%s%d", str, va_arg(pa, int));
+			break;
+		default:
+			break;
 		}
+	str = ", ";
+
 		i++;
 	}
 
 	va_end(pa);
-	putchar('\n');
+	printf("\n");
 }
