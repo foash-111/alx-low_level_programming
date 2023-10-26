733cebe128811a8f582865222796f51ae82accef
foash-111 <mr2221@fayoum.edu.eg>
2023-10-26 11:14:28 +0300

    a function that deletes a node at a given position.

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,50 @@
+#include"lists.h"
+
+/**
+ * delete_nodeint_at_index - ....
+ * @head: pointer to the head
+ * @index: nth index that should I delete
+ * Return: number of nodes
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *current, *checker;
+unsigned int count = 1;
+
+if (head == NULL || *head == NULL)
+	return (-1);
+
+current = *head;
+checker = current;
+
+if (index == 0)
+{
+if (current->next == NULL)
+	*head = NULL;
+else
+	*head = current->next;
+free(current);
+return (1);
+}
+else
+{
+	checker = current->next;
+while (current && count <= index)
+{
+if (count == index)
+{
+if (checker->next == NULL)
+	current->next = NULL;
+else
+	current->next = checker->next;
+free(checker);
+return (1);
+}
+count++;
+current = current->next;
+checker = checker->next;
+}
+}
+return (-1);
+}
