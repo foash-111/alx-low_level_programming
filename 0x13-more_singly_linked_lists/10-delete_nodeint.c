84ca566a8d732908fef40b8d8b3780ffe7ca2b7e
foash-111 <mr2221@fayoum.edu.eg>
2023-10-26 11:15:51 +0300

    a function that deletes a node at a given position.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *current, *checker;
 unsigned int count = 1;
 
-if (head == NULL || *head == NULL)
+if (head == NULL)
 	return (-1);
 
 current = *head;
