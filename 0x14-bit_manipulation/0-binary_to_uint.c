e3f1396660e1009364970019d89102329091a58b
foash-111 <mr2221@fayoum.edu.eg>
2023-10-28 23:37:20 +0200

    function that prints in decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
@@ -7,28 +7,22 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-int i = 0, temp = 0, power = 1;
-unsigned int sum = 0;
-if (b == NULL)
-return (0);
+	unsigned int i = 0, sum = 0;
 
-while (b[i])
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+while (b[i] == '0' || b[i] == '1')
 {
-if (b[i] == '0' || b[i] == '1')
+sum = (sum << 1)  + (b[i] - '0');
+
 i++;
-else
-return (0);
 }
-i--;
-
-while (b[i])
-{
-temp = b[i] - '0';
-
-sum += temp *power;
-power *= 2;
-i--;
-}
+if (b[i] != '\0')
+	return (0);
 
 return (sum);
+
 }
