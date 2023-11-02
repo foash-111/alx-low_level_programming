299e9e726a64fc01631b5a924016b244e00e5f15
foash-111 <mr2221@fayoum.edu.eg>
2023-11-02 23:24:09 +0200

    function that copy from file to anthor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ char buffer[1024];
 
 if (argc != 3)
 {
-write(STDERR_FILENO, "Usage: cp file_from file_to\n", 29);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
 fd_file_from = open(argv[1], O_RDONLY);
 if (fd_file_from == -1)
