a327dda697565c49ab636d348bb342951227a7fa
foash-111 <mr2221@fayoum.edu.eg>
2023-11-02 23:10:59 +0200

    function that copy from file to anthor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ if (fd_file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
-fd_file_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+fd_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_file_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
