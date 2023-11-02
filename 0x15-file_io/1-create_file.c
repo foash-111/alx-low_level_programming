d772a78680e99bcff9dd8424b4a0c7f79bef5154
foash-111 <mr2221@fayoum.edu.eg>
2023-11-02 15:03:15 +0200

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,12 +21,16 @@ fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 
-if (text_content)
+if (text_content == NULL)
 {
+	text_content = "";
+}
+
 written_chars = write(fd, text_content, len);
 if (written_chars == -1 || written_chars != len)
 return (-1);
-}
+
+close(fd);
 
 return (1);
 
