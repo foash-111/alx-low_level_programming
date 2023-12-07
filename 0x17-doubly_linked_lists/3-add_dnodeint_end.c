a93f950d6774eba156b40f017422234a3ebf1117
foash-111 <mr2221@fayoum.edu.eg>
2023-12-07 23:16:51 +0200

    insert node in the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,27 +9,27 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
-	dlistint_t *current = NULL;
+        dlistint_t *new_node = NULL;
+        dlistint_t *current = NULL;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
+        new_node = malloc(sizeof(dlistint_t));
+        if (new_node == NULL)
+                return (NULL);
 
-	new_node->n = n;
+        new_node->n = n;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		return (*head);
-	}
+        if (*head == NULL)
+        {
+                *head = new_node;
+                new_node->next = NULL;
+                new_node->prev = NULL;
+                return (*head);
+        }
 
 current = *head;
 while (current->next != NULL)
 {
-	current = current->next;
+        current = current->next;
 }
 current->next = new_node;
 new_node->next = NULL;
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include"lists.h"
+/**
+ * insert_dnodeint_at_index - insert_node_at_index
+ * @h: pointer points to the head
+ * @n: data node
+ * @idx: target index .. starting from 0
+ * Return: the address of new node we inserted
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = NULL, *current = NULL;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (*h == NULL)
+	{
+		if (idx == 0)
+		{
+		*h = new_node;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		return (*h);
+		}
+		return (NULL);
+	}
+current = *h;
+while (current)
+{
+	if (count == idx)
+	{
+		if (idx == 0)
+			add_dnodeint(h, n);
+		else if (current->next == NULL)
+			add_dnodeint_end(h, n);
+		else
+		{
+		new_node->next = current;
+		new_node->prev = current->prev;
+		current->prev->next = new_node;
+		current->prev = new_node;
+		}
+		return (new_node);
+	}
+	current = current->next;
+	count++;
+}
+return (NULL);
+}
