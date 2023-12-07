8f588aea68802338db1322a41332af8160809732
foash-111 <mr2221@fayoum.edu.eg>
2023-12-07 20:51:16 +0200

    function returns number of nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include"lists.h"
+/**
+ * dlistint_len - function that counts all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = NULL;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	temp = h;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+
+}
