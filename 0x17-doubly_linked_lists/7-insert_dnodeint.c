63c68420dc687281b62ea6e3c9832552c5e6640e
foash-111 <mr2221@fayoum.edu.eg>
2023-12-07 23:38:06 +0200

    insert node in the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,15 +18,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	if (h == NULL)
 	return (NULL);
-	if (*h == NULL)
-	{
-		if (idx == 0)
-		{
-		*h = new_node;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		return (*h); }
-		return (NULL); }
 current = *h;
 while (current)
 {
