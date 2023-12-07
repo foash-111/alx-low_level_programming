fdc446ac157cf63446aca15f94cb11339ddf188c
foash-111 <mr2221@fayoum.edu.eg>
2023-12-08 00:28:35 +0200

    delete node in the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,36 +16,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-
-	if (*h == NULL)
+	if (*h == NULL && idx == 0)
 	{
-		if (idx == 0)
-		{
 		*h = new_node;
 		new_node->next = NULL;
 		new_node->prev = NULL;
 		return (*h); }
-		return (NULL); }
 current = *h;
 while (current)
 {
 	if (count == idx)
 	{
 		if (idx == 0)
-			add_dnodeint(h, n);
+		{
+			new_node->prev = NULL;
+			new_node->next = *h;
+			(*h)->prev = new_node;
+			*h = new_node; }
 		else if (current->next == NULL)
-			add_dnodeint_end(h, n);
+		{
+			new_node->next = NULL;
+			new_node->prev = current;
+			current->next = new_node; }
 		else
 		{
 		new_node->next = current;
 		new_node->prev = current->prev;
 		current->prev->next = new_node;
-		current->prev = new_node;
-		}
+		current->prev = new_node; }
 		return (new_node);
 	}
-	current = current->next;
-	count++;
+	current = current->next, count++;
 }
 return (NULL);
 }
