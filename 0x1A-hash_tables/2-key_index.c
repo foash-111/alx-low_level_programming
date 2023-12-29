62b404b65e21287940ae5fc9e0e5bba3d17a0ceb
foash-111 <mr2221@fayoum.edu.eg>
2023-12-29 17:07:12 +0200

    hash-table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -25,11 +25,11 @@ unsigned long int hash_djb2(const unsigned char *str)
  * @key: string that we store in our node->key
  * @size: the size of the array of linked-lists
  * Return: the index which we store
- * 
+ *
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = hash_djb2(key);
+	unsigned long int index = hash_djb2(key);
 
-    return (index % size);
+	return (index % size);
 }
