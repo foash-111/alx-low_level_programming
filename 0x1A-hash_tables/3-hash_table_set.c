c8e5e0f417197c5933a0c29cf9893ee071325497
foash-111 <mr2221@fayoum.edu.eg>
2023-12-30 00:27:39 +0200

    hash-table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,37 @@
 #include "hash_tables.h"
 
 /**
+* _strdup - duplicate
+* @str: string we want to dublicate
+* Return: pointer to dublicated string
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len = 0, i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	ptr = malloc((sizeof(char) * len) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (i < len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
+}
+/**
  * hash_table_set - hash_table_set
  * @ht: pointer to hash-table
  * @key: string in a node
@@ -14,8 +45,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int index = key_index((const unsigned char *)key, ht->size);
 hash_node_t *head, *new_node =  malloc(sizeof(hash_node_t));
 
-new_node->key = strdup(key);
-new_node->value = strdup(value);
+new_node->key = (char *)key;
+new_node->value = _strdup((char *)value);
 new_node->next = NULL;
 
 head = ht->array[index];
