dded3114babbd465768cd27bee7e2d358ba7ddc1
foash-111 <mr2221@fayoum.edu.eg>
2023-12-29 17:06:11 +0200

    hash-table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 0;
+	int c = 0;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
+
+
+/**
+ * key_index - key_index
+ * @key: string that we store in our node->key
+ * @size: the size of the array of linked-lists
+ * Return: the index which we store
+ * 
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int index = hash_djb2(key);
+
+    return (index % size);
+}
