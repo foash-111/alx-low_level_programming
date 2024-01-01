15556efeaa723a843831ecb8fdc2d5af960992ff
foash-111 <mr2221@fayoum.edu.eg>
2024-01-01 22:11:44 +0200

    hash-table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,22 +43,12 @@ char *_strdup(char *str)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index = 0;
-hash_node_t *current = NULL, *new_node =  NULL;
+hash_node_t *new_node =  NULL;
 
 if (ht == NULL || value == NULL)
 return (0);
 index = key_index((const unsigned char *)key, ht->size);
-current = ht->array[index];
-while (current)
-{
-	if (strcmp(current->key, key) == 0)
-	{
-	free(current->value);
-	current->value = strdup(value);
-	return (1);
-	}
-	current = current->next;
-}
+
 new_node = malloc(sizeof(hash_node_t));
 
 if (!new_node)
